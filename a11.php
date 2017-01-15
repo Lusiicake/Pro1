bd4fe521efed6357b4bc1060d5226cb61549f967
Lusiicake <natchaya.pond.toom@hotmail.com>
2017-01-15 17:28:33 +0700

    Add files via upload

---
+++ a11.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Chocolate</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" />
+<link href="css/tabMenu.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+		<!-- �ʴ��Ϳ࿤�ྨ -->
+
+<div align="center">
+   
+		<!-- ���ٻ��ҹ���ش�ͧ���ྨ -->
+  <div id="head">
+  <img src="images/B05.jpg" width="900" height="300" align="center">
+
+
+  <!--div �Դ������-->
+	<!-- div nav �Դ -->
+        <div class="navbar">
+        <div class="holder">
+        <ul>
+        	<li><a href="index1.html"><img  src="images/B1.jpg" width="180px" height="45px"></a></li>
+			<li><a href="C1.html"><img  src="images/B2.jpg" width="180px" height="45px"></a></li>
+			<li><a href="C2.html"><img  src="images/B3.jpg" width="180px" height="45px"></a></li>
+			<li><a href="C3.html"><img  src="images/B4.jpg" width="180px" height="45px"></a></li>
+            <li><a href="Profile.html"><img  src="images/B5.jpg" width="180px" height="45px"></a></li>
+          </ul>
+        
+        
+        </div><!-- end holder div -->
+</div><!-- end navder div -->
+     <!-- div nav  �Դ-->
+        <!-- ����ǹ������ ��� Navigation ������ -->
+		
+        <div id="containner"> 	
+        	<div id="main">
+            	<div class="menu_right">
+            	  <h1 class="yellow">&nbsp;</h1>
+          	      
+            	    <form id="form1" name="form1" method="POST" action= "<?php echo $loginFormAction; ?>">
+            	            <center><p>
+            	              <label for="user">Username:</label>
+            	              <input type="text" name="user" id="user" />
+          	              </p>
+           	            <p>
+           	              <label for="pass">Password:</label>
+           	              <input type="password" name="pass" id="pass" />
+           	              </p></center>
+           	            <p>
+            	              <p> <h3><lefe>&nbsp;&nbsp; &nbsp; &nbsp;&nbsp; <a href="index2.html"> �������к�</a>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="index1.html"> ¡��ԡ</a></p></h3>
+           	              </p>
+           	          </form>
+						<p> <center>&nbsp;&nbsp; <a href="form.php"> ��Ѥ���Ҫԡ</a></center></p>
+						
+						 <h1 class="green">&nbsp;</h1>
+					  <ul>
+                   
+                   		  </ul>
+							 <a href="http://www.facebook.com/"><img src="images/rujr.png" width="200" height="200" /></a>   
+						            	   
+							 <a href="https://line.me/th/"><img src="images/Open_dtac.png" width="200" height="170" /></a></td>
+						
+            	             <a href="https://www.google.co.th/"><img src="images/google_lupa.jpg" width="200" height="120" /></a>
+          	                        	                  	       
+      
+            	      <td>
+          	      </tr>
+            	    <tr>
+            	      
+          	      </tr>
+          	    </table>
+           	  </div>
+
+            	<!-- ����¡�÷������������ҧ������ -->
+            	<div id="left"> 
+                	
+                    <!-- ����ǹ navigation ������ -->
+                    <div id="navigation">                   
+                      					  					      
+
+                      <!-- ��ǹ������٪�͡��ŵ-->
+                      <h1 class="green">&nbsp;</h1>
+					  <br>
+					  <ul>
+                   
+                   		<li><a href="http://cooking.kapook.com/view117002.html">��͡��ŵ����</a></li>
+                   		<li><a href="https://cookpad.com/th/recipes/941106-%E0%B8%84%E0%B8%B8%E0%B8%81%E0%B8%81%E0%B8%B5%E0%B9%89%E0%B8%8A%E0%B9%87%E0%B8%AD%E0%B8%81%E0%B9%82%E0%B8%81%E0%B9%81%E0%B8%A5%E0%B8%95%E0%B8%8A%E0%B8%B4%E0%B8%9B">�ء����͡��ŵ�Ի</a></li>
+                        <li><a href="https://cookpad.com/th/recipes/660100-easy-chocolate-mousse">Easy Chocolate Mousse</a></li> 
+                        <li><a href="https://cookpad.com/th/recipes/640961-%E0%B8%99%E0%B8%B2%E0%B8%A1%E0%B8%B0-%E0%B8%AD%E0%B8%B1%E0%B8%A5%E0%B8%A1%E0%B8%AD%E0%B8%A5%E0%B8%94%E0%B9%8C-%E0%B8%8A%E0%B8%AD%E0%B8%84%E0%B9%82%E0%B8%81%E0%B9%81%E0%B8%A5%E0%B8%95-%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B9%86-%E0%B8%AA%E0%B9%84%E0%B8%95%E0%B8%AA%E0%B9%8C%E0%B8%88%E0%B8%B8%E0%B9%8B%E0%B8%A1-nama-chocolate">���� �����Ŵ� �ͤ��ŵ </a></li>
+						<li><a href="https://cookpad.com/th/recipes/276374-flourless-chocolate-cake">Flourless chocolate cake</a></li>
+						<li><a href="https://cookpad.com/th/recipes/1230057-istyle-cafe-honey-toast">Istyle Cafe Honey toast</a></li>
+						<li><a href="https://cookpad.com/th/recipes/1045611-%E0%B8%9A%E0%B8%B2%E0%B8%99%E0%B8%B2%E0%B8%99%E0%B9%88%E0%B8%B2%E0%B9%82%E0%B8%A3%E0%B8%A5">�ҹҹ�����</a></li>
+						<li><a href="https://cookpad.com/th/recipes/914719-%E0%B8%9A%E0%B8%A3%E0%B8%B2%E0%B8%A7%E0%B8%99%E0%B8%B5%E0%B9%88%E0%B8%AB%E0%B8%99%E0%B9%89%E0%B8%B2%E0%B8%81%E0%B8%A3%E0%B8%AD%E0%B8%9A%E0%B9%80%E0%B8%99%E0%B8%B7%E0%B9%89%E0%B8%AD%E0%B8%AB%E0%B8%99%E0%B8%B6%E0%B8%9A%E0%B9%86%E0%B8%89%E0%B9%88%E0%B8%B3%E0%B9%86">���ǹ��˹�ҡ�ͺ</a></li>
+						<li><a href="https://cookpad.com/th/recipes/898757-energy-boost-%E0%B8%A2%E0%B8%B2%E0%B8%A1%E0%B9%80%E0%B8%8A%E0%B9%89%E0%B8%B2">Energy Boost </a></li>
+						<li><a href="https://cookpad.com/th/recipes/799600-%E0%B8%97%E0%B8%B2%E0%B8%A3%E0%B9%8C%E0%B8%95%E0%B8%8A%E0%B8%AD%E0%B8%84%E0%B9%82%E0%B8%81%E0%B9%81%E0%B8%A5%E0%B8%95%E0%B8%81%E0%B8%B2%E0%B8%99%E0%B8%B2%E0%B8%8A-%E0%B8%87%E0%B9%88%E0%B8%B2%E0%B8%A2%E0%B9%86-%E0%B8%AA%E0%B9%84%E0%B8%95%E0%B8%AA%E0%B9%8C%E0%B8%88%E0%B8%B8%E0%B9%8B%E0%B8%A1">����쵪ͤ��ŵ�ҹҪ</a></li>
+                   	  </ul>
+					  
+					   <!--��þ�ҡó��ҡ��-->
+					  <h1 class="red"></h1>
+					  <br>
+					  <iframe src="http://www.tmd.go.th/daily_forecast_forweb.php" width="200" height="260" scrolling="no" frameborder=0></iframe>
+
+					   <!--���ԡ�-->
+                      <h1 class="yellow">&nbsp;</h1>
+                      <ul>				  
+						<embed src="http://blog.roodo.com/aaa011/39ecd9ba.swf"width="140"height="127"
+						type="application/x-shockwave-flash"wmode="transparent"/></embed>
+
+
+                   	  </ul> 
+	
+  					
+					<!-- ��ǹ���¢ͧ���٪�͡��ŵ-->
+                  </div> <!-- end navigator -->
+                   
+                   <!-- ����ö������������´���ҧ��� ����ͧ����������ҧ������ͧ͢���ྨ�� -->
+              </div> <!-- end left--> 
+                    
+                    
+                    
+                    
+              <!-- ����¡�÷������������ҧ������ -->     
+           	  <div id="right" align="center">                      
+
+                <div id="content" class="green">
+           
+				   <div class="holder">
+				    <div id="content" class="green">
+                  
+				  <h1 align="center"><font color="black">�������Ңͧ ��͡��ŵ(Coh-Co-Late) </h1></font>
+				<font size="2" face="MS Sans Serif" style="font-family:TH SarabunPSK; font-size: 16pt;">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+					��͡��ŵ�١�鹾��ҵ�����ͧ�ѹ�շ������ ��ѧ���¾�йҧ����;ѵ��������Ի�� �繼ż�Ե�����ҡ���索ͧ�鹤��� (cacao) 㹻����͹��鹢ͧ��ջ����ԡ� �Ѵ����㹵�С�� Theobroma cacao 
+					����� "�������觷��෾" ��������á������ѡ�Ӫ�͡��ŵ�����¸�����ҳ����������硫�� �������ԡҡ�ҧ �������������������� ��Ъ���ͫ෤ ������¸�����������ԡ�
+					������ҹ��������紤����Һ����Ǽ���Ѻ����ͧ��ا���ª�Դ���ͷ�������ͧ����������ʢ����͹ �͡�ҡ���Сͺ����� ���Ǫ�͡��ŵ�ѧ����Ǣ�ͧ�Ѻ�Զժ��Ե�ԧ��ʹ�����ѧ������										
+					������� (�.�.250-900) �繪��ҵ��á�������ѡ�ҹ�Ѵਹ�����鹾������Ѻ�ͧ�鹤����¾ǡ�� ��ӵ鹤����Ҩҡ��ҽ���л�١������ǹ��ѧ��ҹ ���͡�ѡ���������������ѡ��ҧ ���Ǻ�ҧ
+					����ѧ���������˹��� ��ҡ��������ͧ����������Ҽ����� ��¾�ԡ� �駢���⾴ ���������ͧ������͡��ŵ�ʫҺ����տͧ��ͧ<ul>
+
+				</td></ul>
+				
+					&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+					������� (�.�.250-900) �繪��ҵ��á�������ѡ�ҹ�Ѵਹ�����鹾������Ѻ�ͧ�鹤����¾ǡ�� ��ӵ鹤����Ҩҡ��ҽ���л�١������ǹ��ѧ��ҹ ���͡�ѡ���������������ѡ��ҧ ���Ǻ�ҧ
+					����ѧ���������˹��� ��ҡ��������ͧ����������Ҽ����� ��¾�ԡ� �駢���⾴ ���������ͧ������͡��ŵ�ʫҺ����տͧ��ͧ<ul>
+				</ul>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 �������Ǥ��ʵ�ȵ���ɷ�� 14 �ҳҨѡâͧ����ͫ෤��ͺ��ͧ��鹷����ǹ�˭�ͧ���¸�����������ԡ� �������ͧ��ǧ������������ͧ�Ѩ�غѹ���¡��� ��硫�� �Ե��
+				����ͫ෤����͢�����紤��ҡѺ���������Ъ��ҵ���� ����ѧ���¡�纤�Һ�óҡ�èҡ�����ͧ�ͧ�������������紤�������᷹����Թ
+				����ͫ෤����������͡��ŵ�������ǡѺ��������ؤ�á�»�ا�ʪҵ�������Ң�鹴�������ͧ�� �����������ԡ����¹���ѧ������û�١���¡���������������ӵ�šѹ<ul>
+				</ul>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 ���ҡѹ��� �������ؤ�����ԡ�ͺ������͡��ŵ�ѹ����о���ɢ�з���ô����;��ǧ��й��������ѹ�ҡ��ǹ����ͫ෤ ��ôҼ�黡��ͧ�дѺ�٧ ��� �������٧ 
+				��о�ͤ�ҷ����˹���յ���ҹ�鹷�����Է������������ͧ�����ѡ����Է����� ��͡��ŵ�պ��ҷ�Ӥѭ㹾Ըբͧ�Ҫǧ�������ʹ� ��������紤���������ͧ�ѡ����෾��� ��д���㹾Ը��Ӥѭ<ul>
+				</ul>
+				&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				����Ѻ����Ңͧ���ͪ�͡��ŵ����ѧ�������͸Ժ��������Ѵ���դ���������ͧ�ҧ �ҧ�á�繤ӷ��ѹ�Ҩҡ����� "��ͤ��Ѩ" ��������ҫ�����¶֧ �Ҵ�����͡��ŵ���¡ѹ �ա�ҧ˹��͸Ժ����ҹ�Ҩ��Ҩҡ���������蹡ѹ 
+				��� " chocol" ����� ��͹ ����Ѻ����� "atl" �ͧ�ͫ෤�������� ��� ��������ѹ�֧�����繤���� chocolatl ������� chocolate ���������û</font><ul>
+				</ul>
+		 
+					<p align="center">
+					<img border="0" src="images/gg.jpg" width="476px" height="300px"></td>
+                  
+			
+						
+							
+                </div> 
+                </div> 
+                  <!-- end content-->
+           
+                    
+                    
+                                     
+                                     
+               <!-- ����ö������������´���ҧ��� ����ͧ����������ҧ�����ͧ͢���ྨ�� -->                       
+              </div> 
+           	  <!-- end right-->
+                
+       	  </div> <!-- end main-->
+            
+  </div> <!-- end containner -->
+		
+        <!-- ����ǹ��ҧ�ش�ͧ���ྨ -->        
+        <div id="footer">
+		  <p>Rmutsv <br>
+		  Powered By Kritsada Sentohmud</p>
+           
+ <marquee bgcolor="#00FF00" direction="lefe" scrollamount="5" width="100%">�Թ�յ�͹�Ѻ������˹�������� ��͡��ŵ...</marquee>		  
+        </div>   
+		
+</body>
+</html>
